9814c8c3fe7e054e7643f20596940c2e324c1d40
VK-learner <kulkarnivaibhav8856@gmail.com>
2026-04-30 15:35:23 +0530

    feat: add method2 frequency counter using dict.get()

--- p12.py
+++ p12.py
@@ -13,7 +13,7 @@
 
 #  Store Frequency in Dictionary
 # Time Complexity = 𝑂(𝑛)
-# Space Complexity = O(n) ---> i worst case if all keys are unique
+# Space Complexity = O(n) ---> in worst case if all keys are unique
 # num = [5,6,7,7,1,9,111,1,1,5,1,1]
 # dictionary = {}
 # for i in range(len(num)):
@@ -23,6 +23,22 @@
 #         dictionary[num[i]] = 1
 # print(dictionary)
 
-# method 2
-num = [5,6,7,7,1,9,111,1,1,5,1,1]
-n = len(num)
+# method 2 using hashmap
+# hashmap = {'a':1,'b':2,'c':3}
+# print(hashmap.get('d','not found this key value'))
+
+# hashmap = {'a':1,'b':2,'c':3}
+# try:
+#     print(hashmap['d'])
+# except KeyError:
+#     print("not found this key value")
+
+def frequency(num):
+    n = len(num)
+    hashmap = {}
+    for i in range(0,len(num)):
+        hashmap[num[i]] = hashmap.get(num[i],0)+1
+    return hashmap
+print(frequency([5,6,7,7,1,9,111,1,1,5,1,1]))
+
+
